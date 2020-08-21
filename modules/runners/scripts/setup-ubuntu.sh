63d3e13db804ed5af10de3f3ae5b60eb233ab6b5
Vincent Brison <vincent.brison@fabernovel.com>
2020-08-21 09:28:31 +0200

    Ensure all vm setup succeed

--- modules/runners/scripts/setup-ubuntu.sh
+++ modules/runners/scripts/setup-ubuntu.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+## Ensure all setup succeed
+set -e
+
 while true; do
   case "$1" in
     --token ) token="$2"; shift 2 ;;
@@ -19,7 +23,6 @@ sudo apt-get -y install jq
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo groupadd docker
 sudo usermod -aG docker $USER
 
 ## Auto clean
@@ -32,3 +35,5 @@ tar xzf ./actions-runner-linux-x64-2.169.1.tar.gz
 ./config.sh  --unattended --url https://github.com/$org --token $token --labels docker --name $name
 sudo ./svc.sh install
 sudo ./svc.sh start
+
+exit 0
