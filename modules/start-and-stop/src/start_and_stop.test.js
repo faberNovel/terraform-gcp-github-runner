36eebace5e8210716f9cdd2c2f24f0b096d60629
Vincent Brison <vincent.brison@fabernovel.com>
2020-08-24 18:18:18 +0200

    Add a dummy behavior test

--- modules/start-and-stop/src/start_and_stop.test.js
+++ modules/start-and-stop/src/start_and_stop.test.js
@@ -1,13 +1,31 @@
-const StartAndStop = require(`./start_and_stop.js`);
-var chai = require("chai");
-var chaiAsPromised = require('chai-as-promised');
+const startAndStop = require(`./start_and_stop.js`);
+const sinon = require('sinon');
+const chai = require("chai");
+const chaiAsPromised = require('chai-as-promised');
+const Compute = require('@google-cloud/compute');
+
 chai.use(chaiAsPromised);
 chai.should();
 
 describe('start instance', function () {
     describe('wrong args', function () {
         it('Should error if wrong args', function() {
-            return StartAndStop.startAndStop(null, null).should.rejected;
-        });
-    });
-});
+            return startAndStop.startAndStop(null, null).should.rejected
+        })
+    })
+    describe('happy path 0 VMs', function() {
+        it('Should be fulfilled', function() {
+            const env = "test"
+            const json = {
+                "action": "start",
+                "filter": `labels.env=${env} AND labels.idle=false}`
+            }
+            const jsonBase64 = Buffer.from(JSON.stringify(json)).toString('base64')
+            const payload = {
+                "data": jsonBase64
+            }
+            sinon.stub(Compute.prototype, `getVMs`).callsFake(() => [[]])
+            return startAndStop.startAndStop(payload, null).should.be.fulfilled
+        })
+    })
+})
