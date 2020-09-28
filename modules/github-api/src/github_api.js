6ce9bc5819f0d298068cb20075a381ff51d6671c
Vincent Brison <vincent.brison@fabernovel.com>
2020-09-28 16:12:46 +0200

    Implement all github api calls needed

--- modules/github-api/src/github_api.js
+++ modules/github-api/src/github_api.js
@@ -4,9 +4,11 @@ const GithubHelper = require('./github-helper.js')
 
 module.exports.githubApi = async (data, context) => {
   try {
-    console.log('Hello from github api interface')
-    const token = getRegistrationToken()
-    return Promise.resolve(token)
+    const payload = validatePayload(
+      JSON.parse(Buffer.from(data.data, 'base64').toString())
+    )
+    const apiResult = await getApiResult(payload.action)
+    return Promise.resolve(apiResult)
   } catch (err) {
     console.log(err)
     return Promise.reject(err)
@@ -14,14 +16,59 @@ module.exports.githubApi = async (data, context) => {
 }
 
 module.exports.dev = async () => {
-  const token = await getRegistrationToken()
-  console.log(token)
+  const apiResult = await getApiResult('runners')
+  console.log(apiResult)
+}
+
+async function getApiResult (action) {
+  const apiCall = (function (color) {
+    switch (color) {
+      case 'registration':
+        return getRegistrationToken()
+      case 'remove':
+        return getRemoveToken()
+      case 'runners':
+        return getRunners()
+      default:
+        throw new Error(`action ${action} invalid`)
+    }
+  })(action)
+  return apiCall
 }
 
 async function getRegistrationToken () {
+  console.log('getRegistrationToken...')
   const octokit = await GithubHelper.getOctokit()
   const response = await octokit.actions.createRegistrationTokenForOrg({
     org: ORG
   })
+  console.log('getRegistrationToken done')
   return JSON.stringify(response.data.token)
+}
+
+async function getRemoveToken () {
+  console.log('getRemoveToken...')
+  const octokit = await GithubHelper.getOctokit()
+  const response = await octokit.actions.createRemoveTokenForOrg({
+    org: ORG
+  })
+  console.log('getRemoveToken done')
+  return JSON.stringify(response.data.token)
+}
+
+async function getRunners () {
+  console.log('getRunners...')
+  const octokit = await GithubHelper.getOctokit()
+  const response = await octokit.actions.listSelfHostedRunnersForOrg({
+    org: ORG
+  })
+  console.log('getRunners done')
+  return JSON.stringify(response.data.runners)
+}
+
+function validatePayload (payload) {
+  if (!payload.action) {
+    throw new Error('Attribute \'action\' missing from payload')
+  }
+  return payload
 }
