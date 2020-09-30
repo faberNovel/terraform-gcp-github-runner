7f74231d81d676e7c51141da59d29aeb5ba92ab1
Vincent Brison <vincent.brison@fabernovel.com>
2020-09-30 14:36:13 +0200

    Add cleaner script

--- modules/start-and-stop/src/runner-scripts/start-script.sh
+++ modules/start-and-stop/src/runner-scripts/start-script.sh
@@ -18,6 +18,8 @@ log_error() {
 
 log_debug "start startup script"
 
+RUNNER_USER="ubuntu"
+
 sudo apt-get -y update
 
 ## Minimum tools
@@ -29,7 +31,18 @@ sudo apt-get -y install jq
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $RUNNER_USER
+
+## Auto clean
+cd "/home/$RUNNER_USER"
+CLEANER_FILE="cleaner.sh"
+sudo -u $RUNNER_USER touch $CLEANER_FILE
+> $CLEANER_FILE
+echo "#!/bin/sh" >> $CLEANER_FILE
+echo "if [ $(id -u) -ne 0 ] ; then echo\"Please run as root\" ; exit 1 ; fi" >> $CLEANER_FILE
+echo "rm -r /home/ubuntu/actions-runner/_work/_temp" >> $CLEANER_FILE
+echo "docker image prune -a -f" >> $CLEANER_FILE
+echo "0 */6 * * * sudo sh /home/$RUNNER_USER/$CLEANER_FILE >> /home/$RUNNER_USER/cron-cleaner-log 2>&1" | sudo -u $RUNNER_USER crontab -
 
 log_debug "runner software setup done, start runner registration"
 
@@ -49,12 +62,12 @@ else
 fi
 
 ## Runner
-cd /home/ubuntu
-sudo -u ubuntu mkdir actions-runner && cd actions-runner
-sudo -u ubuntu curl -O -L https://github.com/actions/runner/releases/download/v2.169.1/actions-runner-linux-x64-2.169.1.tar.gz
-sudo -u ubuntu tar xzf ./actions-runner-linux-x64-2.169.1.tar.gz
-sudo -u ubuntu ./config.sh  --unattended --url https://github.com/$GITHUB_ORG --token $REGISTRATION_TOKEN --labels docker --name $HOSTNAME
-sudo -u ubuntu ./run.sh &
+cd "/home/$RUNNER_USER"
+sudo -u $RUNNER_USER mkdir actions-runner && cd actions-runner
+sudo -u $RUNNER_USER curl -O -L https://github.com/actions/runner/releases/download/v2.169.1/actions-runner-linux-x64-2.169.1.tar.gz
+sudo -u $RUNNER_USER tar xzf ./actions-runner-linux-x64-2.169.1.tar.gz
+sudo -u $RUNNER_USER ./config.sh  --unattended --url https://github.com/$GITHUB_ORG --token $REGISTRATION_TOKEN --labels docker --name $HOSTNAME
+sudo -u $RUNNER_USER ./run.sh &
 
 log_debug "end startup script with success"
 
