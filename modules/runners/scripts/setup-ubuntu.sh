e54f0b3a259746a33535f45ab2ef3d5101eb1577
Vincent Brison <vincent.brison@fabernovel.com>
2020-09-30 20:30:41 +0200

    Use official and recommended docker install
    As documented by Docker itself.

--- modules/runners/scripts/setup-ubuntu.sh
+++ modules/runners/scripts/setup-ubuntu.sh
@@ -24,7 +24,19 @@ sudo apt-get -y install \
     jq
 
 ## Docker
-sudo apt install -y docker.io
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
