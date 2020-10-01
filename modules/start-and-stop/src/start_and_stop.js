6450c4f74bcfc0f2284e45f4d8bb41738db7c559
Vincent Brison <vincent.brison@fabernovel.com>
2020-10-01 18:57:52 +0200

    Extract VM creation for readability

--- modules/start-and-stop/src/start_and_stop.js
+++ modules/start-and-stop/src/start_and_stop.js
@@ -1,6 +1,6 @@
 const Compute = require('@google-cloud/compute')
 const { GoogleAuth } = require('google-auth-library')
-const Fs = require('fs')
+const CreateVMHelper = require('./create_vm_helper.js')
 const compute = new Compute()
 const zone = compute.zone(process.env.GOOGLE_ZONE)
 const auth = new GoogleAuth()
@@ -32,80 +32,6 @@ module.exports.dev = async () => {
   const [operation] = await vm.delete()
   await operation.promise()
   console.log('VM deleted')
-}
-
-async function createVm (isIdle, id) {
-  console.log('create VM ...')
-  const [vm, operation] = await zone.createVM(createVmName(id), createVmConfig(isIdle, process.env.GOOGLE_ENV))
-  console.log(vm)
-  console.log('Creating VM ...')
-  await operation.promise()
-  console.log('VM created')
-  return vm
-}
-
-function createVmName (runnerId) {
-  const vmName = `vm-gcp-github-action-runner-${process.env.GOOGLE_ENV}-${runnerId}`
-  console.log(`vm name created : ${vmName}`)
-  return vmName
-}
-
-function createVmConfig (isIdle, env) {
-  const startScript = Fs.readFileSync('runner-scripts/start-script.sh', 'utf8')
-  const stopScript = Fs.readFileSync('runner-scripts/stop-script.sh', 'utf8')
-  const config = {
-    machineType: process.env.RUNNER_MACHINE_TYPE,
-    http: true,
-    disks: [
-      {
-        boot: true,
-        autoDelete: true,
-        initializeParams: {
-          diskSizeGb: '40',
-          sourceImage: 'https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts'
-        }
-      }
-    ],
-    networkInterfaces: [
-      {
-        network: 'global/networks/default'
-      }
-    ],
-    labels: {
-      idle: isIdle,
-      env: env
-    },
-    serviceAccounts: [
-      {
-        email: process.env.RUNNER_SERVICE_ACCOUNT,
-        scopes: [
-          'https://www.googleapis.com/auth/cloud-platform'
-        ]
-      }
-    ],
-    metadata: {
-      items: [
-        {
-          value: process.env.GITHUB_API_TRIGGER_URL,
-          key: 'github-api-trigger-url'
-        },
-        {
-          value: process.env.GITHUB_ORG,
-          key: 'github-org'
-        },
-        {
-          value: startScript,
-          key: 'startup-script'
-        },
-        {
-          value: stopScript,
-          key: 'shutdown-script'
-        }
-      ]
-    }
-  }
-  console.log(`vm config created : ${config}`)
-  return config
 }
 
 async function getInstances (idle) {
