a06b54925ca29f8f48782156fe090dbbfdf29acf
Vincent Brison <vincent.brison@fabernovel.com>
2020-10-06 16:06:01 +0200

    Fully implement image generation

--- image/packer.sh
+++ image/packer.sh
@@ -6,7 +6,7 @@ set -e
 # Printing script usage
 program_name=$0
 usage () {
-  echo "usage: $program_name [--env-file google-env-file.json]"
+  echo "usage: $program_name [--env-file google-env-file.json] [--packer-action build]"
   exit 1
 }
 
@@ -14,6 +14,7 @@ usage () {
 while true; do
   case "$1" in
     --env-file ) env_file="$2"; shift 2 ;;
+    --packer-action ) packer_action="$2"; shift 2;;
     * ) break ;;
   esac
 done
@@ -23,13 +24,17 @@ if [ -z "$env_file" ]; then
     usage
 fi
 
+if [ -z "$packer_action" ]; then
+    usage
+fi
+
 env_file_path=$(realpath $env_file)
 echo $env_file_path
 
 source ../.tools/load-google-env.sh $env_file_path
 source ../.tools/load-default-terraform-env.sh
 
-packer validate \
+packer $packer_action \
   -var region=$GOOGLE_REGION \
   -var zone=$GOOGLE_ZONE \
   -var machine_type=$RUNNER_MACHINE_TYPE \
