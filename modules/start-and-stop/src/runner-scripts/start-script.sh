4341ac7dc982d2b9638236a9bf38129a319997da
Vincent Brison <vincent.brison@fabernovel.com>
2020-10-08 17:32:35 +0200

    Add more labels to vms

--- modules/start-and-stop/src/runner-scripts/start-script.sh
+++ modules/start-and-stop/src/runner-scripts/start-script.sh
@@ -28,6 +28,7 @@ ZONE=$(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instanc
 TOKEN=$(gcloud auth print-identity-token)
 FUNCTION_URL=$(gcloud compute instances describe $HOSTNAME --zone $ZONE --flatten="metadata[github-api-trigger-url]" --format=object)
 GITHUB_ORG=$(gcloud compute instances describe $HOSTNAME --zone $ZONE --flatten="metadata[github-org]" --format=object)
+GOOGLE_ENV=$(gcloud compute instances describe $HOSTNAME --zone $ZONE --flatten="labels[env]" --format=object)
 PAYLOAD="{\"scope\":\"actions\",\"function\":\"createRegistrationTokenForOrg\",\"params\":{\"org\":\"$GITHUB_ORG\"}}"
 REGISTRATION_TOKEN_RESULT=$(curl $FUNCTION_URL -H "Authorization: Bearer $(gcloud auth print-identity-token)" -d $PAYLOAD -H "Content-Type: application/json")
 REGISTRATION_TOKEN=$(jq -r .token <<< "$REGISTRATION_TOKEN_RESULT")
@@ -40,7 +41,7 @@ fi
 
 ## Runner
 cd "/home/$RUNNER_USER/actions-runner"
-sudo -u $RUNNER_USER ./config.sh  --unattended --url https://github.com/$GITHUB_ORG --token $REGISTRATION_TOKEN --labels docker --name $HOSTNAME
+sudo -u $RUNNER_USER ./config.sh  --unattended --url https://github.com/$GITHUB_ORG --token $REGISTRATION_TOKEN --labels docker,$GOOGLE_ENV,$HOSTNAME --name $HOSTNAME
 sudo -u $RUNNER_USER ./run.sh &
 
 log_debug "end startup script with success"
