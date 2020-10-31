cba9697a64d832a413e3e4fbd65f950a0181ec60
Vincent Brison <vincent.brison@gmail.com>
2020-10-31 12:37:46 +0100

    Update deploy script to be more explicit

--- .tools/deploy.sh
+++ .tools/deploy.sh
@@ -5,7 +5,7 @@ set -e
 # Printing script usage
 program_name=$0
 usage () {
-  echo "usage: $program_name --google-env-file google-env-file.json --backend-config-file backend.json --force"
+  echo "usage: $program_name --google-env-file google-env-file.json --backend-config-file backend.json"
   exit 1
 }
 
@@ -14,7 +14,6 @@ while true; do
   case "$1" in
     --google-env-file ) google_env_file="$2"; shift 2 ;;
     --backend-config-file ) backend_config_file="$2"; shift 2 ;;
-    --force ) force=true; shift 1 ;;
     * ) break ;;
   esac
 done
@@ -38,18 +37,20 @@ cd "$project_root_path"
 # Deploy packer image
 echo "Deploying runner image using packer..."
 cd image
+base_packer_cmd="bash packer.sh --env-file $google_env_file_path --packer-action"
+packer_cmd_build="$base_packer_cmd 'build'"
 set +e
-packer_cmd="bash packer.sh --env-file $google_env_file_path --packer-action"
-if [ "$force" = true ]; then
-  packer_cmd="$packer_cmd 'build -force'"
-else
-  packer_cmd="$packer_cmd 'build'"
-fi
-
-if ! eval "$packer_cmd"; then
-  echo "Packer build failed, maybe the image already exists, check logs for more info"
-fi
+eval "$packer_cmd_build"
+packer_cmd_exit_code=$?
 set -e
+if [ $packer_cmd_exit_code -ne 0 ]; then
+  echo "Packer build failed, maybe the image already exists, check logs for more info"
+  read -r -p "Would you like to force deploy the image? (y/n):" input
+  if [ "$input" = "y" ]; then
+    packer_cmd_build_force="$base_packer_cmd 'build -force'"
+    eval "$packer_cmd_build_force"
+  fi
+fi
 echo "Deploying runner image using packer done"
 cd "$project_root_path"
 
