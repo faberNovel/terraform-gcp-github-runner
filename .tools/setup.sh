ba408f86b2f4c351c2369b56db9c711e747194e0
Vincent Brison <vincent.brison@gmail.com>
2020-11-22 12:51:20 +0100

    Fix dev env loading

--- .tools/setup.sh
+++ .tools/setup.sh
@@ -17,12 +17,12 @@ project_root_path=$(realpath "$script_dir/..")
 google_path=$project_root_path/google-dev.tfvars.json
 github_path=$project_root_path/github.auto.tfvars.json
 
+# shellcheck source=.tools/load-default-terraform-env.sh
+source "$project_root_path"/.tools/load-default-terraform-env.sh
 # shellcheck source=.tools/load-google-env.sh
 source "$project_root_path"/.tools/load-google-env.sh "$google_path"
 # shellcheck source=.tools/load-github-env.sh
 source "$project_root_path"/.tools/load-github-env.sh "$github_path"
-# shellcheck source=.tools/load-default-terraform-env.sh
-source "$project_root_path"/.tools/load-default-terraform-env.sh
 
 # clear .env file
 true > .env
@@ -35,8 +35,8 @@ true > .env
     echo "RUNNER_MACHINE_TYPE=$RUNNER_MACHINE_TYPE"
     echo "RUNNER_IDLE_COUNT=$RUNNER_IDLE_COUNT"
     echo "RUNNER_TOTAL_COUNT=$RUNNER_TOTAL_COUNT"
-    echo "RUNNER_SERVICE_ACCOUNT=runner-user@$GOOGLE_PROJECT.iam.gserviceaccount.com"
     echo "RUNNER_TAINT_LABELS=$RUNNER_TAINT_LABELS"
+    echo "RUNNER_SERVICE_ACCOUNT=runner-user@$GOOGLE_PROJECT.iam.gserviceaccount.com"
     echo "SECRET_GITHUB_JSON_RESOURCE_NAME=projects/$GOOGLE_PROJECT/secrets/github-json/versions/latest"
     echo "SECRET_GITHUB_JSON_ID=github-json"
     echo "GITHUB_API_TRIGGER_URL=https://$GOOGLE_REGION-$GOOGLE_PROJECT.cloudfunctions.net/github_api_function"
