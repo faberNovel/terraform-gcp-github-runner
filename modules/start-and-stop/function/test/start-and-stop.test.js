4b3875d244951a4a6971df20c9e296116d311a10
Vincent Brison <vincent.brison@gmail.com>
2020-11-25 19:34:00 +0100

    Add some start and stop payload tests

--- modules/start-and-stop/function/test/start-and-stop.test.js
+++ modules/start-and-stop/function/test/start-and-stop.test.js
@@ -15,8 +15,8 @@ describe('start and stop tests', () => {
       return startAndStop.startAndStop(null, null).should.rejected
     })
   })
-  describe('When start payload', () => {
-    it('should trigger scale', async () => {
+  describe('When create all non idle runners payload', () => {
+    it('should create all non idle runners', async () => {
       const payload = makePayload('create_all_non_idle_runners')
       const scaleHelperMock = sandbox.mock(scaleHelper)
       scaleHelperMock.expects('scaleUpAllNonIdlesRunners').resolves().once()
@@ -26,11 +26,22 @@ describe('start and stop tests', () => {
       sandbox.verifyAndRestore()
     })
   })
-  describe('When stop payload', () => {
-    it('should trigger stop', async () => {
+  describe('When delete all non idle runners payload', () => {
+    it('should delete all non idle runners', async () => {
       const payload = makePayload('delete_all_non_idle_runners')
       const scaleHelperMock = sandbox.mock(scaleHelper)
-      scaleHelperMock.expects('scaleDownAllNonIdlesRunners').resolves().once()
+      scaleHelperMock.expects('scaleDownAllNonIdlesRunners').withExactArgs(false).resolves().once()
+
+      await startAndStop.startAndStop(payload, makeEvent())
+
+      sandbox.verifyAndRestore()
+    })
+  })
+  describe('When force delete all non idle runners payload', () => {
+    it('should force delete all non idle runners', async () => {
+      const payload = makePayload('force_delete_all_non_idle_runners')
+      const scaleHelperMock = sandbox.mock(scaleHelper)
+      scaleHelperMock.expects('scaleDownAllNonIdlesRunners').withExactArgs(true).resolves().once()
 
       await startAndStop.startAndStop(payload, makeEvent())
 
@@ -42,6 +53,17 @@ describe('start and stop tests', () => {
       const payload = makePayload('healthcheck')
       const healthCheckMock = sandbox.mock(healthCheck)
       healthCheckMock.expects('removeOfflineGitHubRunners').resolves().once()
+
+      await startAndStop.startAndStop(payload, makeEvent())
+
+      sandbox.verifyAndRestore()
+    })
+  })
+  describe('When renew idle runners payload', () => {
+    it('Should trigger renew idle runners', async () => {
+      const payload = makePayload('renew_idle_runners')
+      const scaleHelperMock = sandbox.mock(scaleHelper)
+      scaleHelperMock.expects('renewIdleRunners').resolves().once()
 
       await startAndStop.startAndStop(payload, makeEvent())
 
