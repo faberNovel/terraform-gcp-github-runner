2364c7a239b73463d0ddaefa97770e014deaed8c
Vincent Brison <vincent.brison@fabernovel.com>
2020-12-01 11:28:02 +0100

    Let renew idle runner count real vms

--- modules/start-and-stop/function/src/renew-runner.js
+++ modules/start-and-stop/function/src/renew-runner.js
@@ -1,4 +1,5 @@
 const scaleHelper = require('./scale-helper')
+const getRunnerHelper = require('./get-runner-helper')
 const chalk = require('chalk')
 
 module.exports.renewIdleRunners = renewIdleRunners
@@ -7,8 +8,10 @@ async function renewIdleRunners () {
   console.info('renew idle runners...')
   const idle = true
   const force = true
+  const currentIdleRunners = await getRunnerHelper.getRunnersVms(idle)
+  const currentIdleRunnersCount = currentIdleRunners.length
   const targetCount = scaleHelper.getTargetRunnersCount(idle)
-  await scaleHelper.scaleDownRunners(idle, targetCount, force)
+  await scaleHelper.scaleDownRunners(idle, currentIdleRunnersCount, force)
   await scaleHelper.scaleUpRunners(idle, targetCount)
   console.info(chalk.green('idle runners renewed'))
 }
--- modules/start-and-stop/function/test/renew-runner.test.js
+++ modules/start-and-stop/function/test/renew-runner.test.js
@@ -3,26 +3,40 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const renewRunnerHelper = require('../src/renew-runner')
 const scaleHelper = require('../src/scale-helper')
+const getRunnerHelper = require('../src/get-runner-helper')
 
 chai.should()
 chai.use(chaiAsPromised)
 
 describe('Renew runners tests', () => {
   afterEach(function () {
-    sandbox.verify()
-    sandbox.restore()
+    sandbox.verifyAndRestore()
   })
 
   describe('When renew idle runners', () => {
     it('should renew all idle runners', async () => {
       const idle = true
+      const currentIdleRunnerCount = 3
       const targetIdleRunnerCount = 2
       const force = true
       const scaleHelperMock = sandbox.mock(scaleHelper)
+      sandbox.stub(getRunnerHelper, 'getRunnersVms').withArgs(idle).resolves(makeVms(currentIdleRunnerCount))
       scaleHelperMock.expects('getTargetRunnersCount').returns(targetIdleRunnerCount)
-      scaleHelperMock.expects('scaleDownRunners').withExactArgs(idle, targetIdleRunnerCount, force).once()
+      scaleHelperMock.expects('scaleDownRunners').withExactArgs(idle, currentIdleRunnerCount, force).once()
       scaleHelperMock.expects('scaleUpRunners').withExactArgs(idle, targetIdleRunnerCount).once()
-      renewRunnerHelper.renewIdleRunners()
+
+      await renewRunnerHelper.renewIdleRunners()
     })
   })
 })
+
+function makeVms (count) {
+  const vms = []
+  for (let index = 0; index < count; index++) {
+    const vm = {
+      name: `vm-${index}`
+    }
+    vms.push(vm)
+  }
+  return vms
+}
