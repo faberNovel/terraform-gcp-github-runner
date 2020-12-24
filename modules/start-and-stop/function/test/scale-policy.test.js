791e2ec9ac45391808598e93bfa479d235668bb1
Vincent Brison <vincent.brison@fabernovel.com>
2020-12-24 17:44:42 +0100

    Use optional param to setup tests

--- modules/start-and-stop/function/test/scale-policy.test.js
+++ modules/start-and-stop/function/test/scale-policy.test.js
@@ -19,7 +19,7 @@ describe('Scale policy tests', () => {
   describe('When scalling up with threshold already meet', () => {
     it('should not scale up', async () => {
       const nonBusyGcpGitHubRunnersCount = scalePolicySettings.scaleUpNonBusyRunnersTargetCount()
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, 0, 1)
+      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, { runnersMaxCount: 0, scaleDownNonBusyRunnersChunckSize: 1 })
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleUpRunners').never()
       scalePolicy.scaleUp()
@@ -29,7 +29,7 @@ describe('Scale policy tests', () => {
   describe('When scalling up with threshold not meet and no runner slot available', () => {
     it('should scale up', async () => {
       const nonBusyGcpGitHubRunnersCount = scalePolicySettings.scaleUpNonBusyRunnersTargetCount() - 1
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, 1, 1)
+      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, { runnersMaxCount: 1, scaleDownNonBusyRunnersChunckSize: 1 })
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleUpRunners').withExactArgs(1).once()
       scalePolicy.scaleUp()
@@ -39,7 +39,7 @@ describe('Scale policy tests', () => {
   describe('When scalling up with threshold not meet and no runner slot available', () => {
     it('should not scale up', async () => {
       const nonBusyGcpGitHubRunnersCount = scalePolicySettings.scaleUpNonBusyRunnersTargetCount() - 1
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 1, 1, 1)
+      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 1, { runnersMaxCount: 1, scaleDownNonBusyRunnersChunckSize: 1 })
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleUpRunners').withExactArgs(0).once()
       scalePolicy.scaleUp()
@@ -49,7 +49,7 @@ describe('Scale policy tests', () => {
   describe('When scalling down with threshold already meet', () => {
     it('should not scale down', async () => {
       const nonBusyGcpGitHubRunnersCount = 0
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 1, 1, 1)
+      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 1, { runnersMaxCount: 1, scaleDownNonBusyRunnersChunckSize: 1 })
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleDownRunners').never()
       scalePolicy.scaleDown()
@@ -60,7 +60,11 @@ describe('Scale policy tests', () => {
     it('should scale down according scaleDownMaxCount setting', async () => {
       const scaleDownMaxCount = 3
       const nonBusyGcpGitHubRunnersCount = 6
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, nonBusyGcpGitHubRunnersCount, nonBusyGcpGitHubRunnersCount, scaleDownMaxCount)
+      stubExternalDependencies(
+        nonBusyGcpGitHubRunnersCount,
+        nonBusyGcpGitHubRunnersCount,
+        { runnersMaxCount: nonBusyGcpGitHubRunnersCount, scaleDownNonBusyRunnersChunckSize: scaleDownMaxCount }
+      )
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleDownRunners').withExactArgs(scaleDownMaxCount).once()
       scalePolicy.scaleDown()
@@ -71,7 +75,11 @@ describe('Scale policy tests', () => {
     it('should scale down according available runner count', async () => {
       const scaleDownMaxCount = 3
       const nonBusyGcpGitHubRunnersCount = 2
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, nonBusyGcpGitHubRunnersCount, nonBusyGcpGitHubRunnersCount, scaleDownMaxCount)
+      stubExternalDependencies(
+        nonBusyGcpGitHubRunnersCount,
+        nonBusyGcpGitHubRunnersCount,
+        { runnersMaxCount: nonBusyGcpGitHubRunnersCount, scaleDownNonBusyRunnersChunckSize: scaleDownMaxCount }
+      )
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleDownRunners').withExactArgs(nonBusyGcpGitHubRunnersCount).once()
       scalePolicy.scaleDown()
@@ -81,7 +89,7 @@ describe('Scale policy tests', () => {
   describe('When scalling down with threshold not meet and no runner online', () => {
     it('should not scale down', async () => {
       const nonBusyGcpGitHubRunnersCount = 1
-      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, 1, 1)
+      stubExternalDependencies(nonBusyGcpGitHubRunnersCount, 0, { runnersMaxCount: 1, scaleDownNonBusyRunnersChunckSize: 1 })
       const scalHelperMock = sandbox.mock(scaleHelper)
       scalHelperMock.expects('scaleDownRunners').withExactArgs(0).once()
       scalePolicy.scaleDown()
@@ -89,7 +97,12 @@ describe('Scale policy tests', () => {
   })
 })
 
-function stubExternalDependencies (nonBusyRunnersCount, runnersCount, runnersMaxCount, scaleDownNonBusyRunnersChunckSize) {
+function stubExternalDependencies (nonBusyRunnersCount, runnersCount, scalePolicyCustomSettings) {
+  const scalePolicyDefaultSettings = {
+    runnersMaxCount: 1,
+    scaleDownNonBusyRunnersChunckSize: 1
+  }
+  const scalePolicyMergedSettings = { ...scalePolicyDefaultSettings, ...(scalePolicyCustomSettings || {}) }
   sandbox.stub(githubHelper, 'getNonBusyGcpGitHubRunnersCount').resolves(nonBusyRunnersCount)
   const runners = []
   for (let index = 0; index < runnersCount; index++) {
@@ -99,6 +112,6 @@ function stubExternalDependencies (nonBusyRunnersCount, runnersCount, runnersMax
     runners.push(vm)
   }
   sandbox.stub(getRunnerHelper, 'getRunnersVms').resolves(runners)
-  sandbox.stub(scalePolicySettings, 'runnersMaxCount').returns(runnersMaxCount)
-  sandbox.stub(scalePolicySettings, 'scaleDownNonBusyRunnersChunckSize').returns(scaleDownNonBusyRunnersChunckSize)
+  sandbox.stub(scalePolicySettings, 'runnersMaxCount').returns(scalePolicyMergedSettings.runnersMaxCount)
+  sandbox.stub(scalePolicySettings, 'scaleDownNonBusyRunnersChunckSize').returns(scalePolicyMergedSettings.scaleDownNonBusyRunnersChunckSize)
 }
